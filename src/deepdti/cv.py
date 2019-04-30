1b1f8ee392b7d5786c28fdee6b096dfbe122091c
xnuohz <xnuohz@126.com>
2019-04-30 21:00:22 +0800

    classifier cross validation

--- src/deepdti/cv.py
+++ src/deepdti/cv.py
@@ -8,10 +8,10 @@ import pandas as pd
 import configparser
 import tensorflow as tf
 
-from data_utils import get_now, get_data, get_feature, new_pair_fold, new_ligand_fold, new_protein_fold
-from model import CNNClassifier
+from data_utils import get_now, get_feature, new_pair_fold, new_ligand_fold, new_protein_fold
+from model import CNN
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
 def main(argv):
@@ -31,32 +31,38 @@ def main(argv):
     char_smi_set = json.load(open(conf.get('model', 'char_smi')))
     char_seq_set = json.load(open(conf.get('model', 'char_seq')))
 
-    smi_feature, seq_feature = get_data(
-        ligands, proteins, max_smi_len, max_seq_len, char_smi_set, char_seq_set)
-    print(smi_feature.shape, seq_feature.shape, inter.shape)
-
     cv_num = conf.getint('cv', 'cv_num', fallback=5)
-    cv_train, cv_valid = new_pair_fold(inter, cv_num)
+
+    problem_type = conf.getint('cv', 'problem_type', fallback=1)
+    if problem_type == 1:
+        cv_train, cv_valid = new_pair_fold(inter, cv_num)
+    elif problem_type == 2:
+        cv_train, cv_valid = new_protein_fold(inter, cv_num)
+    elif problem_type == 3:
+        cv_train, cv_valid = new_ligand_fold(inter, cv_num)
+
+    print(cv_train[0].shape, cv_valid[0].shape)
 
     sess = tf.InteractiveSession(
         config=tf.ConfigProto(allow_soft_placement=True))
-    model = CNNClassifier(filter_num=conf.getint('model', 'filter_num'),
-                          smi_window_len=conf.getint(
-                              'model', 'smi_window_len'),
-                          seq_window_len=conf.getint(
-                              'model', 'seq_window_len'),
-                          max_smi_len=max_smi_len,
-                          max_seq_len=max_seq_len,
-                          char_smi_set_size=len(char_smi_set),
-                          char_seq_set_size=len(char_seq_set),
-                          embed_dim=conf.getint('model', 'embed_dim'))
+    model = CNN(filter_num=conf.getint('model', 'filter_num'),
+                smi_window_len=conf.getint('model', 'smi_window_len'),
+                seq_window_len=conf.getint('model', 'seq_window_len'),
+                max_smi_len=max_smi_len,
+                max_seq_len=max_seq_len,
+                char_smi_set_size=len(char_smi_set),
+                char_seq_set_size=len(char_seq_set),
+                embed_dim=conf.getint('model', 'embed_dim'))
 
     for cv_id in range(cv_num):
         print('start cv', cv_id)
         model_path = os.path.join(
             conf.get('model', 'path', fallback='tmp'), 'cv-' + str(cv_id) + '.model')
-        trainX, trainy, validX, validy = get_feature(
-            inter, smi_feature, seq_feature, cv_train[cv_id], cv_valid[cv_id])
+        trainX, trainy = get_feature(
+            ligands, proteins, inter, cv_train[cv_id], max_smi_len, char_smi_set, max_seq_len, char_seq_set)
+        validX, validy = get_feature(
+            ligands, proteins, inter, cv_valid[cv_id], max_smi_len, char_smi_set, max_seq_len, char_seq_set)
+        print(trainX.shape, trainy.shape, validX.shape, validy.shape)
         model.train(sess, trainX, trainy, validX, validy,
                     nb_epoch=conf.getint('model', 'num_epoch'),
                     batch_size=conf.getint('model', 'batch_size'),
--- src/deepdti/data_utils.py
+++ src/deepdti/data_utils.py
@@ -20,71 +20,68 @@ def label_sequence(line, max_seq_len, smi_ch_ind):
     return X
 
 
-def get_data(ligands, proteins, max_smi_len, max_seq_len, char_smi_set, char_seq_set):
-    smi_feature = []
-    seq_feature = []
+def get_feature(ligands, proteins, inter, coords, max_smi_len, char_smi_set, max_seq_len, char_seq_set):
+    X, y = [], []
+    for row, col in coords:
+        smi = ligands.iloc[row, 1]
+        seq = proteins.iloc[col, 1]
+        try:
+            smi_vector = label_smiles(smi, max_smi_len, char_smi_set)
+            seq_vector = label_sequence(seq, max_seq_len, char_seq_set)
+            X.append([smi_vector, seq_vector])
+            y.append(inter[row][col])
+        except Exception:
+            continue
+    return np.asarray(X), np.asarray(y).reshape([-1, 1])
 
-    for idx, row in ligands.iterrows():
-        smi_feature.append(label_smiles(
-            row[1], max_smi_len, char_smi_set))
 
-    for idx, row in proteins.iterrows():
-        seq_feature.append(label_sequence(
-            row[1], max_seq_len, char_seq_set))
-
-    return np.asarray(smi_feature), np.asarray(seq_feature)
-
-
-def get_feature(inter, smi_feature, seq_feature, train_idx, valid_idx):
-    trainX, trainy, validX, validy = [], [], [], []
-
-    for row, col in train_idx:
-        trainX.append([smi_feature[row], seq_feature[col]])
-        trainy.append(inter[row][col])
-
-    for row, col in valid_idx:
-        validX.append([smi_feature[row], seq_feature[col]])
-        validy.append(inter[row][col])
-
-    return np.asarray(trainX), np.reshape(trainy, [-1, 1]), np.asarray(validX), np.reshape(validy, [-1, 1])
+def get_coord(inter):
+    ''' default pos : neg = 1 : 15 '''
+    pos_row, pos_col = np.where(inter == 1)
+    pos_coord = np.asarray(list(zip(pos_row, pos_col)))
+    neg_row, neg_col = np.where(inter == 0)
+    neg_coord = np.asarray(list(zip(neg_row, neg_col)))
+    select = np.random.choice(
+        range(len(neg_coord)), size=15 * len(pos_coord))
+    neg_coord = np.asarray(neg_coord)[select]
+    return pos_coord, neg_coord
 
 
 def new_pair_fold(inter, cv_num):
-    inter_row, inter_col = np.where(np.isnan(inter) == False)
-    coord = np.asarray(list(zip(inter_row, inter_col)))
+    pos_coord, neg_coord = get_coord(inter)
     kfold = KFold(n_splits=cv_num, shuffle=True, random_state=1)
+    coord = np.concatenate([pos_coord, neg_coord], 0)
     cv_train, cv_valid = [], []
     for train, valid in kfold.split(coord):
         cv_train.append(coord[train])
         cv_valid.append(coord[valid])
-    return cv_train, cv_valid
+    return np.asarray(cv_train), np.asarray(cv_valid)
 
 
 def new_ligand_fold(inter, cv_num):
-    inter_row, inter_col = np.where(np.isnan(inter) == False)
-    coord = np.asarray(list(zip(inter_row, inter_col)))
+    pos_coord, neg_coord = get_coord(inter)
     kfold = KFold(n_splits=cv_num, shuffle=True, random_state=1)
+    coord = np.concatenate([pos_coord, neg_coord], 0)
     cv_train, cv_valid = [], []
 
     d_num = np.shape(inter)[0]
     for train, valid in kfold.split(range(d_num)):
         cv_train.append(np.asarray([t for t in coord if t[0] in train]))
         cv_valid.append(np.asarray([t for t in coord if t[0] in valid]))
-    print(np.shape(cv_train), np.shape(cv_valid))
-    return cv_train, cv_valid
+    return np.asarray(cv_train), np.asarray(cv_valid)
 
 
 def new_protein_fold(inter, cv_num):
-    inter_row, inter_col = np.where(np.isnan(inter) == False)
-    coord = np.asarray(list(zip(inter_row, inter_col)))
+    pos_coord, neg_coord = get_coord(inter)
     kfold = KFold(n_splits=cv_num, shuffle=True, random_state=1)
+    coord = np.concatenate([pos_coord, neg_coord], 0)
     cv_train, cv_valid = [], []
-
     t_num = np.shape(inter)[1]
+
     for train, valid in kfold.split(range(t_num)):
         cv_train.append(np.asarray([t for t in coord if t[1] in train]))
         cv_valid.append(np.asarray([t for t in coord if t[1] in valid]))
-    return cv_train, cv_valid
+    return np.asarray(cv_train), np.asarray(cv_valid)
 
 
 def get_now():
